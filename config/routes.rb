648235ab15120d15abae37909cc217e31160fb85
Saki421 <u944467h@ecs.osaka-u.ac.jp>
2024-02-29 23:46:45 +0900

    Release to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'directions/new', to: 'directions#new'
   post '/directions', to: 'directions#create', as: 'create_direction'
   get '/result/:start_location/:end_location/:waypoint', to: 'directions#result', as: 'result'
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :tweets 
